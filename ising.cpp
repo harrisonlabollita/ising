3e4a907ee2fa579c9d48f88edf6687837a133ee0
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-09-14 12:37:22 -0700

    init function in c++

---
+++ ising.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h> 
+
+
+// initialize the state matrix of classical spins
+void init(int L) {
+	int state[L][L] = {}
+	for(int i=0; i<L; i++) {
+		for(int j=0; i<L; j++) {
+			state[i][j] = (rand() > 0.5) ? 1 : -1
+		}
+	}
+} // end function init
