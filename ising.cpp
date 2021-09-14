581bfa598c0145fa42f4c44087e088eb460839ba
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-09-14 14:34:25 -0700

    2d arrays cant be passed to functiosn

--- ising.cpp
+++ ising.cpp
@@ -3,33 +3,32 @@
 #include <iostream>
 using namespace std;
 
-
-
-
-// initialize the state matrix of classical spins
-void init(int L, int **state) {
-	for(int i=0; i<L; i++) {
-		for(int j=0; i<L; j++) {
-			state[i][j] = (rand() > 0.5) ? 1 : -1;
-		}
-	}
-} // end function init
-
-
-void init(int L, int **);
+//declare functions
+void init(void *state, int L);
 
 // run the main program
-int main() 
-{
+int main() {
 	int L = 10;
-	int **state[L][L] = { {} };
-	init(L, state);
-	for(int i=0; i<L; i++){
-		for(int j=0; j<L; j++){
-			cout<<state[i][j];
-		}
-	cout<<endl;
-	}
-	return 0;
+	int state[L][L] = {0};
+	init(state, L);
+
+	for(int i=0; i<L; i++) {
+		for(int j=0; j<L; j++) {
+			cout << state[i][j] << ' ';
+	        }
+      }
+      return 0;
 }
+
+
+
+// init: initialize the state as matrix of 1's and -1's
+void init(int **state, int L) {
+	for(int i=0; i<L; i++) {
+		for(int j=0; j<L; j++) {
+			state[i][j] = ((double) rand()/RAND_MAX>=0.5) ? 1 : -1;
+		}
+	}
+}
+
 
