38864353771fa2f91df6608ef04cf5edcf9ab33b
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-09-14 12:42:05 -0700

    compiles but seg faults

--- ising.cpp
+++ ising.cpp
@@ -1,12 +1,21 @@
 #include <stdlib.h> 
 
 
+
 // initialize the state matrix of classical spins
 void init(int L) {
-	int state[L][L] = {}
+	int state[L][L] = {};
 	for(int i=0; i<L; i++) {
 		for(int j=0; i<L; j++) {
-			state[i][j] = (rand() > 0.5) ? 1 : -1
+			state[i][j] = (rand() > 0.5) ? 1 : -1;
 		}
 	}
 } // end function init
+
+
+// run the main program
+int main() {
+	int L = 10;
+	init(L);
+}
+
