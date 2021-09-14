75312fd9406c27414cb0cc0e2437f9160ecf3ae0
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-09-14 13:07:25 -0700

    cpp doesn't work

--- ising.cpp
+++ ising.cpp
@@ -1,10 +1,13 @@
+#include <cmath>
 #include <stdlib.h> 
+#include <iostream>
+using namespace std;
+
 
 
 
 // initialize the state matrix of classical spins
-void init(int L) {
-	int state[L][L] = {};
+void init(int L, int **state) {
 	for(int i=0; i<L; i++) {
 		for(int j=0; i<L; j++) {
 			state[i][j] = (rand() > 0.5) ? 1 : -1;
@@ -13,9 +16,20 @@ void init(int L) {
 } // end function init
 
 
+void init(int L, int **);
+
 // run the main program
-int main() {
+int main() 
+{
 	int L = 10;
-	init(L);
+	int **state[L][L] = { {} };
+	init(L, state);
+	for(int i=0; i<L; i++){
+		for(int j=0; j<L; j++){
+			cout<<state[i][j];
+		}
+	cout<<endl;
+	}
+	return 0;
 }
 
