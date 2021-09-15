3e44da29535555401d08618aecbde2b15d040b7a
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-09-14 18:50:19 -0700

    cpp state init works

--- ising.cpp
+++ ising.cpp
@@ -1,16 +1,17 @@
 #include <cmath>
+#include <vector>
 #include <stdlib.h> 
 #include <iostream>
 using namespace std;
 
 //declare functions
-void init(void *state, int L);
+vector<vector<int>> init(int L);
 
 // run the main program
 int main() {
 	int L = 10;
-	int state[L][L] = {0};
-	init(state, L);
+	vector<vector<int>> state;
+	state = init(L);
 
 	for(int i=0; i<L; i++) {
 		for(int j=0; j<L; j++) {
@@ -23,12 +24,13 @@ int main() {
 
 
 // init: initialize the state as matrix of 1's and -1's
-void init(int **state, int L) {
+vector<vector<int>> init(int L) {
+	vector<vector<int>> state;
+	state.resize(L, vector<int>(L, 0));
 	for(int i=0; i<L; i++) {
 		for(int j=0; j<L; j++) {
-			state[i][j] = ((double) rand()/RAND_MAX>=0.5) ? 1 : -1;
+			state[i][j] = ((double) rand()/RAND_MAX>=0.5) ? 1 :-1;
 		}
 	}
+	return state;
 }
-
-
