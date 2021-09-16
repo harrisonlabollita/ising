fcac755be5978ed7655847659a56028d8d8ccf85
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-09-16 11:51:44 -0700

    f90 funcs written

--- ising.f90
+++ ising.f90
@@ -1,60 +1,65 @@
 program main
         implicit none
         integer, parameter:: L = 10
-        integer :: i, j
+        real, parameter :: Jint = 1.0
         integer, dimension(L, L) :: state
 
         state = init(L)
-        do i=1,L
-                do j=1,L
-                        write(*,*) state(i,j)
-                end do
-        end do
+        print *, energy(state, Jint)
+
 contains
-function init(L) result (state)
-        ! initialize a state
-        implicit none
-        integer :: i, j, L
-        integer, dimension(L, L) :: state
-
-        do i=1,L
-                do j=1,L
-                        if (rand() > 0.5) then
-                                state(i, j) = 1
-                        else
-                                state(i, j) = -1
-                        end if
+        function init(L) result (state)
+                implicit none
+                integer :: i, j, L
+                integer, dimension(L, L) :: state
+                do i=1,L
+                        do j=1,L
+                                if (rand() > 0.5) then
+                                        state(i,j) = 1
+                                else
+                                        state(i,j) = -1
+                                end if
+                        end do
                 end do
-        end do
-end function init
+        end function init
 
-
-function neighbors(state, i, j) result (neigh)
-        ! calculate interaction with neighbors
-        implicit none
-        integer :: L, i, j, neigh
-        integer, dimension(L, L) :: state
-        L = size(state, 1)
-        ! TODO: calculate left, right, up, down
-        neigh = left+right+up+down
-end function neighbors
-
-
-
-function energy(state, J) result (E)
-        ! calculate the energy of a given state
-        implicit none
-        integer :: L, i, j
-        real :: J
-        real :: E = 0.0
-        integer, dimension(L, L) :: state
-        L = size(state, 1)
-        do i=1,L
-                do j=1,L
-                E -= J*state*neighbors(state, i, j)
+        function energy(state, Jint) result (ene)
+                implicit none
+                integer :: i, j, L
+                integer, dimension(:, :) :: state
+                real :: Jint, ene
+                L = size(state, 1)
+                ene = 0.0
+                do i=1,L
+                        do j=1,L
+                                ene = ene + Jint*real(state(i,j))*real(neighbors(state, i,j))*real(neighbors(state, i,j))
+                        end do
                 end do
-        end do
-end function energy
+         end function energy
 
-
+         function neighbors(state, i, j) result (neigh)
+                 implicit none
+                 integer :: L, neigh, i, j
+                 integer :: ileft, iright, jup, jdn
+                 integer, dimension(:, :) :: state
+                 L = size(state, 1)
+                 ileft = mod(i-1, L)
+                 iright = mod(i+1, L)
+                 if (ileft == 0) then
+                         ileft = L
+                 end if
+                 iright = mod(i+1, L)
+                 if (iright == L+1) then
+                         iright = 1
+                 end if
+                 jup = mod(j-1, L)
+                 if (jup == 0) then 
+                         jup = L
+                 end if
+                 jdn = mod(j+1, L)
+                 if (jdn == L+1) then
+                         jdn = 1
+                 end if
+                 neigh = state(ileft, j)+state(iright, j)+state(i,jup)+state(i,jdn)
+         end function neighbors
 end program main
